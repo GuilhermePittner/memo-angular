f046236bd54d24ba49a005ff721ad812a77df097
guilherme_pittner <guilherme_pittner@ethercity.tech>
2025-08-26 22:04:20 -0300

    using ngModel

--- src/app/components/thoughts/create-thought/create-thought.component.ts
+++ src/app/components/thoughts/create-thought/create-thought.component.ts
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateThoughtComponent implements OnInit {
 
+  thoughts_example = {
+    id: '0',
+    content: "Mussum Ipsum, cacilds vidis litro abertis. Detraxit consequat et quo num tendi nada. Todo mundo vê os porris que eu tomo, mas ninguém vê os tombis que eu levo! Cevadis im ampola pa arma uma pindureta. Paisis, filhis, espiritis santis.",
+    author: 'Mussum',
+    model: ''
+  }
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  saveThought() {
+    alert('saved!');
+  }
+
+  cancelPost() {
+    alert('aborting....');
   }
 
 }
